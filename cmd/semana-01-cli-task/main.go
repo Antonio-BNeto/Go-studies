70036d3939c7b0b888cc78ec56926e137699d8a9
Antonio Barros <antonio.barros.alcantara.neto@ccc.ufcg.edu.br>
2026-07-30 13:58:39 -0300

    docs: Adds studies from week 1.

---
+++ cmd/semana-01-cli-task/main.go
@@ -0,0 +1,154 @@
+package main
+
+// ================================================
+
+// func main() {
+// 	i, j := 42, 2701
+
+// 	p := &i         // point to i
+// 	fmt.Println(*p) // read i through the pointer
+// 	*p = 21         // set i through the pointer
+// 	fmt.Println(i)  // see the new value of i
+
+// 	p = &j         // point to j
+// 	*p = *p / 37   // divide j through the pointer
+// 	fmt.Println(j) // see the new value of j
+
+// }
+
+// ================================================
+
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
+// func main() {
+// 	fmt.Println(Vertex{1, 2})
+// }
+
+// ================================================
+
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
+// func main() {
+// 	v := Vertex{1, 2}
+// 	v.X = 4
+// 	fmt.Println(v.X)
+// }
+
+// ================================================
+
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
+// func main() {
+// 	v := Vertex{1, 2}
+// 	p := &v
+// 	p.X = 1e9
+// 	fmt.Println(v)
+// }
+
+// ================================================
+
+// type Vertex struct {
+// 	X, Y int
+// }
+
+// var (
+// 	v1 = Vertex{1, 2}  // has type Vertex
+// 	v2 = Vertex{X: 1}  // Y:0 is implicit
+// 	v3 = Vertex{}      //X:0 and Y:0
+// 	p  = &Vertex{1, 2} // has type *Vertex
+// )
+
+// func main() {
+// 	fmt.Println(v1, p, v2, v3)
+// }
+
+// ================================================
+
+// func main() {
+// 	var a [2]string
+// 	a[0] = "Hello"
+// 	a[1] = "World"
+// 	fmt.Println(a[0], a[1])
+// 	fmt.Println(a)
+
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+// 	fmt.Println(primes)
+// }
+
+// ================================================
+
+// func main() {
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+// 	var s []int = primes[1:4] //a[low : hight]
+// 	fmt.Println(s)
+// }
+
+// ================================================
+
+// func main() {
+// 	names := [4]string{
+// 		"John",
+// 		"Paul",
+// 		"George",
+// 		"Ringo",
+// 	}
+// 	fmt.Println(names)
+
+// 	a := names[0:2]
+// 	b := names[1:3]
+// 	fmt.Println(a, b)
+
+// 	b[0] = "XXX"
+// 	fmt.Println(a, b)
+// 	fmt.Println(names)
+// }
+
+// ================================================
+
+// func main() {
+// 	q := []int{2, 3, 5, 11, 13}
+// 	fmt.Println(q)
+
+// 	r := []bool{true, false, true, true, false, true}
+// 	fmt.Println(r)
+
+// 	s := []struct {
+// 		i int
+// 		b bool
+// 	}{
+// 		{2, true},
+// 		{3, false},
+// 		{5, true},
+// 		{7, true},
+// 		{11, false},
+// 		{13, true},
+// 	}
+// 	fmt.Println(s)
+// }
+
+// ================================================
+
+// func main() {
+// 	s := []int{2, 3, 5, 7, 11, 13}
+
+// 	s = s[1:4]
+// 	fmt.Println(s)
+
+// 	s = s[:2]
+// 	fmt.Println(s)
+
+// 	s = s[1:]
+// 	fmt.Println(s)
+// }
+
+// ================================================
